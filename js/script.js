46535d88a832569f10b3fc6cf2fa0f5607386fde
比屋定秀太 <hiyajoshuta@hiyateishuutanoMacBook-Air.local>
2021-03-12 13:25:43 +0900

    テスト

---
+++ js/script.js
@@ -0,0 +1,91 @@
+$(function() {
+  $('.fa-bars').click(function() {
+    $('.header-hide').slideDown();
+  });
+  $('.fa-angle-down').click(function() {
+    $('.header-hide').slideUp();
+  });
+  $('#top-btn').click(function() {
+    $('html,body').animate({
+      'scrollTop':0
+    },500)
+  });
+  $(function () {
+    $('a[href^="#"]').click(function () {
+      var speed = 500;
+      var href = $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({ 
+        scrollTop: position 
+      }, speed, "swing");
+      return false;
+    });
+  });
+  $('.works-box-item-inner').hover(
+    function() {
+      $(this).find('.hide').slideDown();
+    },
+    function() {
+      $(this).find('.hide').slideUp();
+    }
+  );
+  $('.works-icon').click(function() {
+    $('.active').removeClass('active');
+    var clickindex = $('.works-icon').index($(this));
+    $('.works-box-item').eq(clickindex).addClass('active');
+  });
+  $('.about').find('.fab').hover(
+    function() {
+      $(this).animate({
+        'font-size':'24px'
+      },100)
+    },
+    function() {
+      $(this).animate({
+        'font-size':'20px'
+      },100)
+    }
+  );
+});
+
+$(function() {
+  var header01 = $('.header-inner-wrap01');
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 10) {
+      header01.css('background-color','white');
+    }else {
+      header01.css('background-color','transparent');
+    }
+  });
+
+  var skill = $('.skill-inner-box');
+  skill.hide();
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 700) {
+      skill.fadeIn(800);
+    }else {
+      skill.fadeOut(800);
+    }
+  });
+  var service = $('.service-inner-box-item');
+  service.hide();
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 1200) {
+      service.fadeIn(1000);
+    }else {
+      service.fadeOut(1000);
+    }
+  });
+  var works = $('.works-img');
+  works.hide();
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 2000) {
+      works.fadeIn(1000);
+    }else {
+      works.fadeOut(1000);
+    }
+  });
+
+});
+
